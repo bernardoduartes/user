cceb1355944eab56858f8b5d96e7c829310b1313
@bernardoduartes <bernardoduartes@gmail.com>
2024-02-27 20:23:25 -0300

    feat: loggin controllers

--- src/main/java/com/shizuca/user/controller/UserController.java
+++ src/main/java/com/shizuca/user/controller/UserController.java
@@ -3,11 +3,14 @@ package com.shizuca.user.controller;
 import com.shizuca.user.dao.UserDTO;
 import com.shizuca.user.model.User;
 import com.shizuca.user.service.UserService;
+import jakarta.websocket.server.PathParam;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequestMapping("user")
 public class UserController {
@@ -17,11 +20,19 @@ public class UserController {
 
     @GetMapping
     public List<User> getAll(){
+        log.info("Getting all users");
         return userService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable(name = "id") final Long userId){
+        log.info("Getting user by id");
+        return userService.getById(userId);
     }
 
     @PostMapping
     public Long create(@RequestBody UserDTO userDTO){
+        log.info("Posting a user");
         return userService.save(userDTO);
     }
 }
